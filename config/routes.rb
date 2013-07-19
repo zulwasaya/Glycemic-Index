a78305efe3c6454f02c5f26573a5185518b17587
Zul Wasaya <zul.wasaya@gmail.com>
2013-07-19 15:15:40 +0100

    commit changes from rubymine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 GlycemicIndex::Application.routes.draw do
+  resources :foods
+
+
+  resources :categories
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
