057cfc6f6f0a784056d2c5a2518030dcdc70b2d6
Zul Wasaya <zul.wasaya@gmail.com>
2013-07-19 14:44:20 +0100

    initial merge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlycemicIndex::Application.config.secret_token = 'a987db76d3e4a05232641bfb9e532e79b0d2c77ab4f2f4de83f2ae4a3b240fbc84cfad850b52ca4e753c4d2bbfd95d63e164154a68febea91e277bd4f5efe254'
