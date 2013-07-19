34d0bd6bb27ec605662765f99b66dfe1441edbbe
Zul Wasaya <zul.wasaya@gmail.com>
2013-07-19 18:09:54 +0100

    fixed bugs in show

--- db/migrate/20130719140342_create_foods.rb
+++ db/migrate/20130719140342_create_foods.rb
@@ -2,7 +2,7 @@ class CreateFoods < ActiveRecord::Migration
   def change
     create_table :foods do |t|
       t.string :name
-      t.string :gi
+      t.integer :gi
       t.integer :category_id
 
       t.timestamps
